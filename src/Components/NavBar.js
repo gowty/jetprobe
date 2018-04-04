cd5c39bb1d9cae3973321dc700f32b4bed96293c
Unknown <867573@gmail.com>
2018-04-05 00:34:36 +0530

    all components are added

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,40 +1,20 @@
 import React, { Component } from 'react';
-import {Navbar,Panel,Row} from 'react-bootstrap';
 import '../Css/App.css';
 
 class NavBar extends Component {
    render() {
        return (
              <div>
-             <Panel className="sideNav">
-             <Panel.Body>
-             <div className="sidebar">
-              <Row style={{marginTop:"30px"}}>
-              <i className="fas fa-home"></i>
-              </Row>
-              <Row style={{marginTop:"30px"}}>
-              <i className="fab fa-dribbble"></i>
-              </Row >
-              <Row style={{marginTop:"30px"}}>
-              <i className="far fa-clipboard"></i>
-              </Row>
-              <Row style={{marginTop:"30px"}}>
-              <i className="fas fa-folder"></i>
-              </Row>
-              </div>
-             </Panel.Body>
-             </Panel>
-             <Navbar style={{backgroundColor:"#fff"}}>
-               <Navbar.Header>
-                  <Navbar.Brand>
-                    <div><i className="fas fa-bars" style={{paddingleft:"-20px"}}></i></div>
-                  </Navbar.Brand>
-               </Navbar.Header>
-              <Navbar.Collapse>
-                 <Navbar.Text pullRight><i className="fas fa-user fa-2x"></i>&nbsp;&nbsp;Cameron Svensson</Navbar.Text>
-              </Navbar.Collapse>
-            </Navbar>
-
+             <div className="nav-bar">
+             <a><i className="fas fa-bars"></i></a>
+              <p><i className="fas fa-user fa-2x"></i>&nbsp;&nbsp;&nbsp;Cameron Svensson &nbsp;&nbsp;&nbsp;&nbsp;<i className="fas fa-caret-down"></i></p>
+             </div>
+             <div className="side-bar">
+             <a><i style={{marginTop:"40px"}} className="fas fa-home"></i></a>
+             <a style={{borderRight: "2px solid #1e73fc"}}><i style={{color:"#1e73fc"}} className="fab fa-dribbble"></i></a>
+             <a><i className="far fa-clipboard"></i></a>
+             <a><i className="fas fa-folder"></i></a>
+             </div>
              </div>
        );
    }
